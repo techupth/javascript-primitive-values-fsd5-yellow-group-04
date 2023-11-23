de345548b6231a9c8e06588649f68026d6f1f2b1
I3AIVK <wacharapol.kh@gmail.com>
2023-11-23 15:11:05 +0700

    feat:add complete bank

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalProductPrices = 27000;
+let discount = 25 / 100;
+let actualPrice = totalProductPrices - totalProductPrices * discount;
+console.log(actualPrice);
